306f14ed518d5b762287c32ba0e8bd811ffe6e7e
AOKI Yuuto <aoki@limbate.com>
2017-03-01 18:51:47 +0900

    support template sorting for partial dependency

--- lib/zenith.rb
+++ lib/zenith.rb
@@ -2,6 +2,7 @@ require 'yaml'
 require 'erb'
 require 'active_support/all'
 require 'pathname'
+require 'tsort'
 
 module Zenith
   class TemplateBuilder
@@ -11,15 +12,11 @@ module Zenith
       @base_dir = Pathname.new(base_dir)
       @main_file_name = main_file_name
       @erb = erb
+      @partial_cache = {}
     end
 
     def build
-      YAML.load(full_template)['template'].to_yaml
-    end
-
-    private
-    def partials
-      Pathname.glob(@base_dir.join("_*#{TEMPLATE_EXT}"))
+      YAML.load(full_template)['template'].deep_dup.to_yaml
     end
 
     def load_template(path)
@@ -27,15 +24,32 @@ module Zenith
       @erb ? eval_erb(data) : data
     end
 
+    private
+    def unordered_partials
+      builder = self
+
+      resolver = PartialResolver.new(@partial_cache) do |name|
+        path = @base_dir.join("_#{name}#{TEMPLATE_EXT}").to_s
+        builder.load_template(path)
+      end
+
+      Pathname.glob(@base_dir.join("_*#{TEMPLATE_EXT}")).map do |path|
+        name = File.basename(path).scan(/^_([^.]+)\./).flatten.first
+        resolver.resolve(name)
+      end
+    end
+
+    def partials
+      PartialGraph.new(unordered_partials).tsort
+    end
+
     def eval_erb(data)
       ERB.new(data).result
     end
 
     def partial_contents
-      partials.map do |template|
-        data = load_template(template)
-        name = File.basename(template).scan(/^_([^.]+)\./).flatten.first
-        [%[#{name}: &#{name}], data.indent(2)].join("\n")
+      partials.map do |partial|
+        [%[#{partial.name}: &#{partial.name}], partial.content.indent(2)].join("\n")
       end
     end
 
@@ -51,6 +65,66 @@ module Zenith
         'template:',
         main.indent(2),
       ].join("\n")
+    end
+  end
+
+  class PartialGraph
+    include TSort
+
+    def initialize(partials)
+      @partials = partials
+    end
+
+    def tsort_each_node(&block)
+      @partials.each(&block)
+    end
+
+    def tsort_each_child(node, &block)
+      node.dependencies.each(&block)
+    end
+  end
+
+  class PartialResolver
+    def initialize(cache, &block)
+      @cache = cache
+      @block = block
+    end
+
+    def resolve(name)
+      if @cache[name]
+        return @cache[name]
+      end
+      @cache[name] = Partial.new(name, @block.call(name), self)
+    end
+  end
+
+  class Partial
+    attr_reader :name, :content, :resolver
+
+    def initialize(name, content, resolver)
+      @name = name
+      @content = content
+      @parsed = YAML.parse(content)
+      @resolver = resolver
+    end
+
+    def dependencies
+      dependency_names.map do |name|
+        @resolver.resolve(name)
+      end
+    end
+
+    def dependency_names(node = nil)
+      node ||= @parsed
+      deps = []
+
+      if node.is_a?(Psych::Nodes::Alias)
+        deps << node.anchor
+      end
+
+      [*deps, *(node.children || []).map {|node|
+        dependency_names(node)
+      }].flatten.uniq
     end
   end
 
@@ -72,6 +146,7 @@ module Zenith
           end
           const_set name.to_sym, klass
           klass.yaml_tag "!#{name}"
+          klass
         end
       end
 
@@ -92,10 +167,14 @@ module Zenith
       !!@installed
     end
 
+    def self.registered_functions
+      @registered_functions
+    end
+
     def self.install!
       return if @installed
       @installed = true
-      FUNCTION_NAMES.each do |name|
+      @registered_functions = FUNCTION_NAMES.map do |name|
         Function.register! name
       end
     end
